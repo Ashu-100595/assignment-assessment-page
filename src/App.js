6815a7a52a28d1df826cef457e253524ef3d96ee
Arpit Kumar <jainarpit547@gmail.com>
2020-10-13 12:04:37 +0530

    homepage done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Navbar from './Components/Navbar/Navbar'
+import Homepage from './MainContainer/HomePage/Homepage';
 
 import './App.css';
 import Footer from './Footer/footer'
@@ -10,7 +11,7 @@ function App() {
     <div className="App">
 
        <Navbar />
-    
+       <Homepage/>
       <Footer/>
 
     </div>
