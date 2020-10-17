dba7a655c91ee2a3a3fe712504bef732d22c1611
Arpit Kumar <jainarpit547@gmail.com>
2020-10-17 21:04:22 +0530

    homepage done

--- src/Container/HomePage/Homepage.js
+++ src/Container/HomePage/Homepage.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import './Homepage.css';
 import Skillcard from '../../Components/SkillCard/skillcard';
+import axios from 'axios';
 
 class Homepage extends React.Component{
+
+    state={
+        carddata:[]
+    }
+
+    componentDidMount(){
+        axios.get('https://5f860417c8a16a0016e6a8d5.mockapi.io/homePage')
+        .then(response =>{
+            this.setState({ carddata: [...response.data] });
+            console.log(this.state);
+        })
+        .catch(err => {
+            console.log('card call failed')
+        })
+    }
 
     render(){
         return(
@@ -10,11 +26,11 @@ class Homepage extends React.Component{
             <h1 className='homeHeading'>Practice Arena</h1>
             <p className='homeSubHeading'>All Skills</p>
             <div className='courseWrapper'>
-               <Skillcard/>
-               <Skillcard/>
-               <Skillcard/>
-               <Skillcard/>
-               <Skillcard/>
+              {
+               this.state.carddata.map(item =>{ 
+                  return <Skillcard key={item.id} image={item.imgUrl} course={item.course} unit={item.units}/>
+               })   
+              }
             </div> 
         </div>
         );
